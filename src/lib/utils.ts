ab023895807d90694078e501f702259635acdd79
heyimcsy <cinnamon1995@naver.com>
2025-11-27 02:51:38 +0900

    [feat] add reusable chart components

--- src/lib/utils.ts
+++ src/lib/utils.ts
@@ -1,6 +1,18 @@
-import { clsx, type ClassValue } from "clsx"
-import { twMerge } from "tailwind-merge"
+import { type ClassValue, clsx } from 'clsx';
+import { twMerge } from 'tailwind-merge';
 
 export function cn(...inputs: ClassValue[]) {
-  return twMerge(clsx(inputs))
+  return twMerge(clsx(inputs));
+}
+
+export function getChangedFields<T extends Record<string, unknown>>(prev: T, next: T) {
+  const diff: Partial<T> = {};
+
+  (Object.keys(next) as (keyof T)[]).forEach((key) => {
+    if (prev[key] !== next[key]) {
+      diff[key] = next[key];
+    }
+  });
+
+  return diff;
 }
