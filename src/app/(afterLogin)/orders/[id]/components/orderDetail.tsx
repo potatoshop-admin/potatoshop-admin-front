35b4f54b6ed7d7ba889aa1ba1f68dd5751467c2d
heyimcsy <cinnamon1995@naver.com>
2025-12-04 21:48:07 +0900

    [refactor] create shared row two buttons

--- src/app/(afterLogin)/orders/[id]/components/orderDetail.tsx
+++ src/app/(afterLogin)/orders/[id]/components/orderDetail.tsx
@@ -5,18 +5,10 @@ import { useRouter } from 'next/navigation';
 import { useDeleteOrder, useGetOrder } from '@/api/orders';
 import { toast } from 'sonner';
 import { useOrderInfo } from '@/stores/useOrderInfo';
-import { RowDisplay } from '@/app/(afterLogin)/components/subtabs/row-form';
-import {
-  Dialog,
-  DialogClose,
-  DialogContent,
-  DialogFooter,
-  DialogTitle,
-  DialogTrigger,
-} from '@/components/ui/dialog';
-import { Button } from '@/components/ui/button';
-import { Order, OrderItem } from '@/types/order';
+import { RowDisplay, RowTwoButtons } from '@/app/(afterLogin)/components/subtabs/row-form';
+import DeleteDialog from '@/app/(afterLogin)/components/subtabs/delete-dialog';
 import Loading from '@/components/ui/loading';
+import { Order, OrderItem } from '@/types/order';
 
 const OrderDetail = ({ id }: { id: string }) => {
   const router = useRouter();
@@ -58,6 +50,9 @@ const OrderDetail = ({ id }: { id: string }) => {
     return sum;
   };
 
+  const editBtn = () => {
+    router.push(`/orders/${id}/edit`);
+  };
   React.useEffect(() => {
     if (data?.data) {
       toast.success(`${data.statusMessage}`);
@@ -74,32 +69,7 @@ const OrderDetail = ({ id }: { id: string }) => {
   }
   return (
     <div className="h-full w-full px-4 py-4 overflow-y-scroll">
-      <Dialog>
-        <div className="w-full h-fit pb-6 flex justify-between items-center">
-          <h1 className="font-24-extrabold sm:text-[28px]">{data?.data.name}</h1>
-          <DialogTrigger asChild>
-            <Button variant="destructive" size="roundedDefault">
-              주뮨 삭제
-            </Button>
-          </DialogTrigger>
-        </div>
-        <DialogContent>
-          <DialogTitle>주문 삭제</DialogTitle>
-          <p>해당 주문을 삭제하시겠습니까?</p>
-          <DialogFooter>
-            <DialogClose asChild>
-              <Button type="button" size="default" variant="outline">
-                취소
-              </Button>
-            </DialogClose>
-            <DialogClose asChild>
-              <Button type="button" size="default" variant="default" onClick={deleteOrder}>
-                확인
-              </Button>
-            </DialogClose>
-          </DialogFooter>
-        </DialogContent>
-      </Dialog>
+      <DeleteDialog title={data?.data.name} item="주문" onClick={deleteOrder} />
       <div className="w-full h-fit rounded-md border-2 border-gray-200 px-4 py-4 space-y-6">
         <RowDisplay
           value={data?.data.user.name}
@@ -151,28 +121,7 @@ const OrderDetail = ({ id }: { id: string }) => {
             </div>
           ))}
         </div>
-        <div className="w-full h-fit flex space-x-2">
-          <Button
-            variant="outline"
-            size="fullWidth"
-            className="flex-1/2"
-            onClick={() => {
-              router.back();
-            }}
-          >
-            뒤로 돌아가기
-          </Button>
-          <Button
-            variant="default"
-            size="fullWidth"
-            className="flex-1/2"
-            onClick={() => {
-              router.push(`/orders/${id}/edit`);
-            }}
-          >
-            주문 정보 수정
-          </Button>
-        </div>
+        <RowTwoButtons confirmTitle="주문정보 수정" cancelTitle="뒤로 돌아가기" onClick={editBtn} />
       </div>
     </div>
   );
